93c3aa2a2121f04e170c501321518eb67fd86d26
Ruochen Liu <ruochen99@berkeley.edu>
2019-04-27 13:03:14 -0700

    left container

--- slidify/src/App.js
+++ slidify/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import RightContainer from "./components/RightContainer.jsx";
+import LeftContainer from "./components/LeftContainer.jsx";
 
 function App() {
   return (
     <div>
+      <LeftContainer />
       <RightContainer />
     </div>
   );
---
+++ slidify/src/components/LeftContainer.jsx
@@ -0,0 +1,106 @@
+
+import React, {Component} from 'react';
+import Slider from '@material-ui/lab/Slider';
+import "../styles/LeftContainer.css";
+// import { createMuiTheme } from '@material-ui/core/styles';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme'
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import green from '@material-ui/core/colors/green';
+// import red from '@material-ui/core/colors/red';
+
+
+
+
+class LeftContainer extends Component {
+  state = {
+    Danceability: 0.5,
+    Duration: 1000,
+    Energy: 0.5,
+    Loudness: -30,
+    Speechiness: 0.33,
+    Tempo: 20,
+    Valence: 0.5
+  };
+
+  handleChange = name => (e, value) => {
+      this.setState({
+        [name]: value
+      });
+  };
+
+  render() {
+    const {Danceability, Duration, Energy, Loudness, Speechiness, Tempo, Valence} = this.state;
+    // const muiTheme = getMuiTheme({
+    //     slider: {
+    //       selectionColor: '#929AA6',
+    //       handleFillColor: '#1B4C59'
+    //     }
+    //   });
+
+    return (
+
+      <div>
+      <div className="slider-container">
+
+        <div className="slider">
+        <div className="title"> Filters </div>
+        <div className="label"> Danceability </div>
+        <Slider
+          min={0}
+          max={1}
+          value={Danceability}
+          onChange={this.handleChange("Danceability")} />
+
+        <div className="label"> Duration </div>
+        <Slider
+          min={0}
+          max={2000}
+          value={Duration}
+          onChange={this.handleChange("Duration")} />
+
+        <div className="label"> Energy </div>
+        <Slider
+          min={0}
+          max={1}
+          value={Energy}
+          onChange={this.handleChange("Energy")} />
+
+        <div className="label"> Loudness </div>
+        <Slider
+          min={-60}
+          max={60}
+          value={Loudness}
+          onChange={this.handleChange("Loudness")} />
+
+        <div className="label"> Speechiness </div>
+        <Slider
+          min={0}
+          max={1}
+          value={Speechiness}
+          onChange={this.handleChange("Speechiness")} />
+
+        <div className="label"> Tempo </div>
+        <Slider
+          min={0}
+          max={50}
+          value={Tempo}
+          onChange={this.handleChange("Tempo")} />
+
+        <div className="label"> Valence </div>
+        <Slider
+          min={0}
+          max={1}
+          value={Valence}
+          onChange={this.handleChange("Valence")} />
+
+        </div>
+
+        </div>
+      </div>
+    );
+  }
+};
+
+
+
+export default LeftContainer;
