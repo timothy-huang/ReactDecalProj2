c42cad43e91091640be28185080e790aabd2493c
Timothy <thuang87@berkeley.edu>
2019-04-30 22:08:13 -0700

    integrating spotify

--- slidify/src/App.js
+++ slidify/src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import RightContainer from "./components/RightContainer.jsx";
 import LeftContainer from "./components/LeftContainer.jsx";
-import PlaybackBar from './components/PlaybackBar.jsx';
+import PlaybackBar from "./components/PlaybackBar.jsx";
 
-function App() {
-  return (
-    <div>
-      <LeftContainer />
-      <RightContainer />
-      <PlaybackBar />
-    </div>
-  );
+import Spotify from "spotify-web-api-js";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: false,
+      devices: [],
+      songs: [],
+      search: "",
+      currentDevice: ""
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <LeftContainer />
+        <RightContainer />
+        <PlaybackBar />
+      </div>
+    );
+  }
 }
 
 export default App;
